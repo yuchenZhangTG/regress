255feb9fcf6dff0b953646a4ee57cdde8fc7ec40
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2022-05-25 21:39:47 -0700

    modify

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
 # TigerGraph GSQL Integral test suites
 ## Usage
-To run test
+1. Pre-requisites: TigerGraph, python3 
+1. Set up the schema and data using
+    ```sh
+    ./setup.sh
+    ``` 
+1. Use to driver to
+    * run single test
+        ```sh
+        ./driver.py globalAccum
+        ``` 
+    * A category of tests
+        ```sh
+        ./driver.py read_query
+        ```
+    * All tests
+        ```sh
+        ./driver.py all
+        ```
+
+## About the schema and data
+The 
+
+## Usage of the test driver `./driver.py`
+./driver.py globalAccum
+```
+
+
 ```
 ./setup.sh
-./driver.py selectStar # Run tests in ./read/e2e/selectStar/
-```
+./driver.py globalAccum
+
+
 
 ## Migrate old tests
 | Old test  | new test |
--- driver.py
+++ driver.py
@@ -3,29 +3,39 @@ import argparse
 from pathlib import Path
 from glob import glob
 import subprocess
+from multiprocessing import Pool, cpu_count
 import os
-
+from time import time
 parser = argparse.ArgumentParser(description='GSQL regress test driver.')
 parser.add_argument('test',  type=str, help='directory name of the test')
 cwd = Path(os.path.realpath(__file__)).parent
 output = cwd/'output'
+threads = cpu_count()
 
 def check(test_dir):
   # check the uniqueness of the test folder
   # check the uniqueness of the query name
   pass
 
-def runQuery(query_file):
-  print(query_file)
-  output_file = output/str(query_file).replace('.gsql', '.log')
-  output_file.parent.mkdir(parents=True, exist_ok=True)  
-  with open(query_file) as fi, open(output_file,'w') as fo:
+def parse(file):
+  subprocess.run(["gsql", "-g", "test_graph", str(file)])
+
+def parse_files(file_list):
+  with Pool(processes=threads) as pool:
+    pool.map(parse, file_list)
+
+def runQuery(file):
+  output_file = output/str(file).replace('.gsql', '.log')
+  output_file.parent.mkdir(parents=True, exist_ok=True)
+  with open(file) as fi, open(output_file,'w') as fo:
     for line in fi:
-      line = line.rstrip()
-      print('\t' + line)
-      subprocess.run(["echo", f"[IL] {line}"], stdout=fo)
+      fo.write(line)
       subprocess.run(["gsql", "-g", "test_graph", line], stdout=fo)
 
+def runQueries(file_list):
+  with Pool(processes=threads) as pool:
+    pool.map(runQuery, file_list)
+        
 def runTest():
   # catogorize the files
   modes = ['all', 'udf', 'gpr', 'interp']
@@ -38,20 +48,26 @@ def runTest():
       files['gsql'].append(f)
     elif name[-1] == 'sh':
       files['sh'].append(f)
+  # parse queries
+  start = time()
+  #for m in ['all', 'udf', 'gpr']:
+  #  parse_files(files[m])  
+  parse_time = time() - start
   # install queries
-  
-  for m in ['all', 'udf', 'gpr']:
-    with open('/tmp/tmp.gsql', 'w') as ftmp:
-      for f in files[m]:
-          with open(f) as f:
-            for line in f: ftmp.write(line)
-    subprocess.run(["gsql", "-g", "test_graph", "/tmp/tmp.gsql"])
-  subprocess.run(["gsql", "-g", "test_graph", "INSTALL QUERY ALL"])
-  
+  start = time()
+  #subprocess.run(["gsql", "-g", "test_graph", "INSTALL QUERY ALL"])
+  install_time = time() - start
   # run queries
+  start = time()
   for f in files['gsql']:
     runQuery(f)
-  
+  run_time = time() - start
+  print('===============')
+  print(f'parse:   {parse_time:.4f} s')
+  print(f'install: {install_time:.4f} s')
+  print(f'run:     {run_time:.4f} s')
+
+
 args = parser.parse_args()
 os.chdir(cwd)
 tests = glob(f'*/{args.test}') 
